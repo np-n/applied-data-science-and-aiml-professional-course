3ae5dc90b8e0d1eab4148aefb068171fa6a9297c
npn-zakipoint <netra.neupane@zakipoint.com>
2026-09-06 20:48:05 +0545

    random forest training - template file

---
+++ random_forest.py
@@ -0,0 +1,18 @@
+from sklearn.models import RandomForestClassifier
+
+# load datasets
+
+
+
+# feature eginnering dataset
+
+
+# train test split
+
+# Train model
+
+
+# Evaluate model performance
+
+
+# test
